1c4d049205f84eee1578319150691b42b670615d
Jelle Breuer <jelle@breuclan.nl>
2024-10-01 23:02:21 +0200

    revert all testbench stuff

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,20 +3,10 @@
 namespace Breuer\PDF\Tests;
 
 use Breuer\PDF\PDFServiceProvider;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Breuer\\PDF\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
     protected function getPackageProviders($app)
     {
         return [

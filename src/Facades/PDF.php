bd7f38345eee61df2a540e1edfbae057a1c8f25a
jbreuer95 <jbreuer95@users.noreply.github.com>
2024-10-03 09:25:42 +0000

    Update facade

--- src/Facades/PDF.php
+++ src/Facades/PDF.php
@@ -5,6 +5,19 @@ namespace Breuer\MakePDF\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static \Illuminate\Http\Response response()
+ * @method static \Illuminate\Http\Response download()
+ * @method static \Breuer\MakePDF\Client view(string $view, array $data = [])
+ * @method static \Breuer\MakePDF\Client html(string $html)
+ * @method static \Breuer\MakePDF\Client name(string $filename)
+ * @method static string chromeDriverBinary()
+ * @method static string chromeHeadlessBinary()
+ * @method static bool onWindows32()
+ * @method static bool onWindows64()
+ * @method static bool onMacARM()
+ * @method static bool onMacIntel()
+ * @method static bool onLinux()
+ *
  * @see \Breuer\MakePDF\Client
  */
 class PDF extends Facade

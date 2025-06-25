f29ccc50d770342702a07c43f31d2881fc2d8ba1
jbreuer95 <5371337+jbreuer95@users.noreply.github.com>
2025-06-25 11:46:09 +0000

    Update facade

--- src/Facades/PDF.php
+++ src/Facades/PDF.php
@@ -27,8 +27,8 @@ use Illuminate\Support\Facades\Facade;
  * @method static bool onMacARM()
  * @method static bool onMacIntel()
  * @method static bool onLinux()
- * @method static int getFreePort(void $start = 9515, void $end = 9999)
- * @method static bool isPortFree(void $port, void $host = '127.0.0.1')
+ * @method static int getFreePort(int $start = 9515, int $end = 9999)
+ * @method static bool isPortFree(int $port, string $host = '127.0.0.1')
  *
  * @see \Breuer\MakePDF\Client
  */
